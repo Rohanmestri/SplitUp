5353b909e84d34f93d1a9b69611002dcabb982d4
Rohanmestri <rmestri98@gmail.com>
2020-09-21 21:04:54 -0400

    Update Dashboard.js

--- app/screens/Dashboard.js
+++ app/screens/Dashboard.js
@@ -1,20 +1,20 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, FlatList } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import firebase from '../firebase/config';
+import FriendsScreen from './FriendsScreen';
 
-function FriendsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Friends!</Text>
-    </View>
-  );
-}
+const DB = firebase.database();
+const user = firebase.auth().currentUser;
+let TransactionsList = {};
+let TransactionDict = [];
+
 
 function GroupScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Group!</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'mediumslateblue'}}>
+      <Text style={{color: "darkslateblue", fontSize: 18, fontWeight: "bold"}}>Groups   </Text>
     </View>
   );
 }
@@ -23,9 +23,9 @@ const Tab = createBottomTabNavigator();
 
 function Dashboard() {
   return (
-      <Tab.Navigator>
-        <Tab.Screen name="Friends" component={FriendsScreen} options={{ tabBarLabel: 'Friends', tabBarIcon: ({ color, size }) => <Ionicons name="md-person" size={32} color="silver" />,}}/>
-        <Tab.Screen name="Groups" component={GroupScreen} options={{ tabBarLabel: 'Groups', tabBarIcon: ({ color, size }) => <Ionicons name="md-people" size={32} color="silver" />,}} />
+      <Tab.Navigator tabBarOptions= {{style: {backgroundColor: 'darkslateblue'}}}>
+        <Tab.Screen name="Friends" component={FriendsScreen} options={{tabBarOptions: {activeTintColor: 'slateblue',inactiveTintColor: 'mediumslateblue'},tabBarLabel: 'Friends', tabBarIcon: ({ color, size }) => <Ionicons name="md-person" size={32} color="mediumslateblue" />,}}/>
+        <Tab.Screen name="Groups" component={GroupScreen} options={{tabBarOptions: {activeTintColor: 'slateblue',inactiveTintColor: 'mediumslateblue'},tabBarLabel: 'Groups', tabBarIcon: ({ color, size }) => <Ionicons name="md-people" size={32} color="mediumslateblue" />,}} />
       </Tab.Navigator>
   );
 }
@@ -33,8 +33,20 @@ function Dashboard() {
 const styles = StyleSheet.create({
     background:{
         justifyContent: "center",
-        alignItems:"center"
+        alignItems:"center",
     },
+});
+
+const friends_styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
 });
 
 export default Dashboard;
